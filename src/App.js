e45fd39b077878575771df5b41de14cde9b9bb55
Vaibhav <vaibhav.chavda@digiqt.com>
2022-12-30 14:51:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import DeveloperCardView from './components/DeveloperCard';
+import "./App.css";
+import DeveloperCardView from "./components/DeveloperCard";
 
 function App() {
   return (
     <div className="App">
-      <DeveloperCardView 
-      dev_name= "Vaibhav Chavda "
-      role = "Software Engineer"
-      skills = "Python, Django"
-      status={false}
+      <DeveloperCardView
+        dev_name="Vaibhav Chavda "
+        role="Software Engineer"
+        skills="Python, Django"
+        status={true}
       />
     </div>
   );
--- src/components/DeveloperCard.js
+++ src/components/DeveloperCard.js
@@ -1,38 +1,38 @@
 import React from "react";
 import logo from "../images/reactimage.jpg";
-import './style.css';
-import moment from 'moment';
+import "./style.css";
+import moment from "moment";
 
 const DeveloperCardView = (props) => {
-    let status_message 
-    let joining_date="2022-01-01"
-    let join_date = moment(joining_date, "YYYYMMDD").fromNow();
+  let status_message;
+  let joining_date = "2020-01-01";
+  let join_date = moment(joining_date, "YYYYMMDD").fromNow();
 
-    if(props.status){
-        status_message = <div>Online</div>
-    }
-    else{
-        status_message = <div>Offline</div>
-    }
+  if (props.status) {
+    status_message = <div>Online</div>;
+  } else {
+    status_message = <div>Offline</div>;
+  }
 
-    return(
-        <div>
-        <div className="cardview">
-            <div className="img">
-                <img src={logo} /><br />
-            </div>
-            <div class="status">
-                <h4>{status_message}</h4>
-            </div>
-            <div className="text">
-                <h1>{props.dev_name}</h1>
-                <h2>{props.role}</h2>
-                <h2>{props.skills}</h2>
-                <h2>{join_date}</h2>
-            </div>
+  return (
+    <div>
+      <div className="cardview">
+        <div className="img">
+          <img src={logo} />
+          <br />
         </div>
+        <div class="status">
+          <h4>{status_message}</h4>
         </div>
-    )
-}
+        <div className="text">
+          <h1>{props.dev_name}</h1>
+          <h2>{props.role}</h2>
+          <h2>{props.skills}</h2>
+          <h2>{join_date}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DeveloperCardView;
